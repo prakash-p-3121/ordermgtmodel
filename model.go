d5c9d97b89af7baf53f58594a670ca96e91730ef
Prakash Premkumar <prakash.p.2292@gmail.com>
2024-03-16 10:06:05 +0530

    selling price field updated.

--- model.go
+++ model.go
@@ -16,15 +16,15 @@ const (
 )
 
 type Order struct {
-	ID           string    `json:"id"`
-	IDBitCount   string    `json:"id-bit-count"`
-	ProductID    string    `json:"product-id"`
-	SellerID     string    `json:"seller-id"`
-	BuyerID      string    `json:"buyer-id"`
-	ListingID    string    `json:"listing-id"`
-	Currency     string    `json:"currency"`
-	SellingPrice float64   `json:"selling-price"`
-	StateID      uint      `json:"state-id"`
-	CreatedAt    time.Time `json:"created-at"`
-	UpdatedAt    time.Time `json:"updated-at"`
+	ID         string    `json:"id"`
+	IDBitCount string    `json:"id-bit-count"`
+	ProductID  string    `json:"product-id"`
+	SellerID   string    `json:"seller-id"`
+	BuyerID    string    `json:"buyer-id"`
+	ListingID  string    `json:"listing-id"`
+	Currency   string    `json:"currency"`
+	Amount     float64   `json:"selling-price"`
+	StateID    uint      `json:"state-id"`
+	CreatedAt  time.Time `json:"created-at"`
+	UpdatedAt  time.Time `json:"updated-at"`
 }
